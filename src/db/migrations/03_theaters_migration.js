b06f63e0e46006eaf7f162200f0a2416f7a7ad8a
Victor Gonzalez <167295210+Plenaire@users.noreply.github.com>
2024-09-18 18:06:19 +0900

    Update 03_theaters_migration.js

--- src/db/migrations/03_theaters_migration.js
+++ src/db/migrations/03_theaters_migration.js
@@ -1,16 +1,16 @@
-exports.up = function(knex) {
-    return knex.schema.createTable('theaters', (table) => {
-        table.increments('theater_id').primary();
-        table.string('name').notNullable();
-        table.string('address_line_1').notNullable();
-        table.string('address_line_2');
-        table.string('city').notNullable();
-        table.string('state').notNullable();
-        table.string('zip').notNullable();
-        table.timestamps(true, true);
-    });
+exports.up = function (knex) {
+  return knex.schema.createTable("theaters", (table) => {
+    table.increments("theater_id").primary();
+    table.string("name");
+    table.string("address_line_1");
+    table.string("address_line_2");
+    table.string("city");
+    table.string("state");
+    table.string("zip");
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable('theaters');
+exports.down = function (knex) {
+  return knex.schema.dropTable("theaters");
 };
