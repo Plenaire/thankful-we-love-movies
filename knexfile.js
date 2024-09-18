a381bd8b2d64705dba02a9914c5e02c0d4e5cddd
Victor Gonzalez <167295210+Plenaire@users.noreply.github.com>
2024-09-18 17:32:04 +0900

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,16 @@
-const knex = require("knex");
-const knexConfig = require("./knexfile");
+const path = require("path");
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
 
-const environment = process.env.NODE_ENV || "development";
-const configuration = knexConfig[environment];
-
-module.exports = knex(configuration);
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+};
