e257f21b521cd8260b3febdc1938e72a8ebe4b2b
Victor Gonzalez <167295210+Plenaire@users.noreply.github.com>
2024-09-18 17:12:16 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const { PORT = 5001 } = process.env;
+const { PORT = 8080} = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
@@ -6,9 +6,9 @@ const knex = require("./db/connection");
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
 knex.migrate
-  .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
-  })
-  .catch(console.error);
+    .latest()
+    .then((migrations) => {
+        console.log("migrations", migrations);
+        app.listen(PORT, listener);
+    })
+    .catch(console.error);
